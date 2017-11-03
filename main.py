cc657505e9ccc8ecc27b6e3479f2d99cac9545c7
Kevin de Haan <kbehaan@gmail.com>
2017-11-03 17:03:31 -0600

    csv formatting

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy as np
 from sklearn.svm import SVC
 
+rawData = np.genfromtxt('./data.csv', delimiter=',', skip_header=True)
+target = rawData[:, [0]].transpose()[0]
+data = rawData[:, 1:]
+
+plant = dict()
+
+plant['data'] = data
+plant['target'] = target
+
+
