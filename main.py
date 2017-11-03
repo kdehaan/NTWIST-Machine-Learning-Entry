90bfc8916a7b8dcf8367c8baddd87278b3f58f38
Kevin de Haan <kbehaan@gmail.com>
2017-11-03 17:56:29 -0600

    some comparison tools

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import r2_score
 
 
 rawData = np.genfromtxt('./data.csv', delimiter=',', skip_header=True)
@@ -31,8 +31,19 @@ x_test = scaler.transform(x_test)
 mlp = MLPRegressor(hidden_layer_sizes=(30, 30, 30))
 mlp.fit(x_train, y_train)
 predictions = mlp.predict(x_test)
-print(y_test[0:5])
-print(predictions[0:5])
-#
-#print(confusion_matrix(y_test, predictions))
-#print(classification_report(y_test, predictions))
+print(y_test[0:5].transpose())
+print(predictions[0:5].transpose())
+difference = np.absolute(y_test-predictions)
+print("Number of tests:" + str(np.alen(difference)))
+print("Sample difference: " + str(difference[0:5]))
+print("Average difference: " + str(np.average(difference)))
+print("Maximum difference: " + str(np.max(difference)))
+print("Minimum difference: " + str(np.min(difference)))
+
+results = np.hstack((predictions, y_test.transpose))
+np.savetxt('results.csv', results, delimiter=',')
+
+# Accuracy metric used:
+# https://en.wikipedia.org/wiki/Coefficient_of_determination
+# closer to 1.0 is better
+print(r2_score(y_test, predictions))
