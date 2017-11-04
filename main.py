ddd89ab3bc00081b1195c7f190c85a929150dea4
Kevin de Haan <kbehaan@gmail.com>
2017-11-04 12:54:19 -0600

    hyperparameter tweaks

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
-from sklearn.svm import SVC
+import time
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
 
 
 rawData = np.genfromtxt('./data.csv', delimiter=',', skip_header=True)
@@ -28,22 +28,34 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
-mlp = MLPRegressor(hidden_layer_sizes=(30, 30, 30))
+start = time.time()
+
+mlp = MLPRegressor(hidden_layer_sizes=(35, 35, 30),
+                   activation='relu',     # relu converges fastest and works well on this dataset
+                   solver='adam',         # can use lbfgs for slightly greater accuracy, or adam for ~1/3 runtime
+                   learning_rate_init=0.0008,  # seems to be the most consistently accurate
+                   )
 mlp.fit(x_train, y_train)
 predictions = mlp.predict(x_test)
-print(y_test[0:5])
-print(predictions[0:5])
+
+end = time.time()
+
+#print(y_test[0:5])
+#print(predictions[0:5])
 difference = np.absolute(y_test-predictions)
 print("Number of tests: " + str(np.alen(difference)))
-print("Sample difference: " + str(difference[0:5]))
+#print("Sample differences: " + str(difference[0:5]))
 print("Average difference: " + str(np.average(difference)))
 print("Maximum difference: " + str(np.max(difference)))
 print("Minimum difference: " + str(np.min(difference)))
+print("Took " + str(end-start) + " seconds to converge")
 results = np.hstack((predictions[np.newaxis].T, y_test[np.newaxis].T, difference[np.newaxis].T))
 
 np.savetxt('results.csv', results, delimiter=',', header='Predicted,Actual,Difference')
+print("Results saved to 'results.csv'")
 
 # Accuracy metric used:
 # https://en.wikipedia.org/wiki/Coefficient_of_determination
 # closer to 1.0 is better
-print(r2_score(y_test, predictions))
+print("R2 Score: " + str(r2_score(y_test, predictions)))
+print("Mean Squared Error: " + str(mean_squared_error(y_test, predictions)))
