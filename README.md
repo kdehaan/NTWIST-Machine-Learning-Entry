c82b7d60a02b8b3346be8cae25d7d53794312637
Kevin de Haan <kbehaan@gmail.com>
2017-11-04 12:59:11 -0600

    readme and sample data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-ntwist
+Kevin de Haan, 2017
+
+Usage instructions:
+
+On linux:
+    extract files to appropriate file location
+    navigate to file location
+    type 'venv/bin/python3.5 main.py'
--- main.py
+++ main.py
@@ -35,27 +35,27 @@ mlp = MLPRegressor(hidden_layer_sizes=(35, 35, 30),
                    solver='adam',         # can use lbfgs for slightly greater accuracy, or adam for ~1/3 runtime
                    learning_rate_init=0.0008,  # seems to be the most consistently accurate
                    )
+
 mlp.fit(x_train, y_train)
 predictions = mlp.predict(x_test)
 
 end = time.time()
 
-#print(y_test[0:5])
-#print(predictions[0:5])
 difference = np.absolute(y_test-predictions)
+
 print("Number of tests: " + str(np.alen(difference)))
-#print("Sample differences: " + str(difference[0:5]))
 print("Average difference: " + str(np.average(difference)))
 print("Maximum difference: " + str(np.max(difference)))
 print("Minimum difference: " + str(np.min(difference)))
 print("Took " + str(end-start) + " seconds to converge")
-results = np.hstack((predictions[np.newaxis].T, y_test[np.newaxis].T, difference[np.newaxis].T))
 
-np.savetxt('results.csv', results, delimiter=',', header='Predicted,Actual,Difference')
-print("Results saved to 'results.csv'")
+results = np.hstack((predictions[np.newaxis].T, y_test[np.newaxis].T, difference[np.newaxis].T))
 
 # Accuracy metric used:
 # https://en.wikipedia.org/wiki/Coefficient_of_determination
 # closer to 1.0 is better
 print("R2 Score: " + str(r2_score(y_test, predictions)))
 print("Mean Squared Error: " + str(mean_squared_error(y_test, predictions)))
+
+np.savetxt('results.csv', results, delimiter=',', header='Predicted,Actual,Difference')
+print("Results saved to 'results.csv'")
